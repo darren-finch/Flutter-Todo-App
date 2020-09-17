595cc270970c1d8af53be302fdca9dd69232e9d4
Darren <finchdarren25@gmail.com>
2020-09-16 22:25:26 -0500

    Nightly commit

--- flutterstuff/lib/screens/todo_list_home/todo_list.dart
+++ flutterstuff/lib/screens/todo_list_home/todo_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutterstuff/data/db/database.dart';
-import 'package:flutterstuff/screens/task_details/task_details_screen.dart';
+import 'package:flutterstuff/screens/todo_list_home/todo_item.dart';
 import 'package:provider/provider.dart';
 
 class TodoList extends StatefulWidget {
@@ -35,34 +35,6 @@ class TodoListState extends State<TodoList> {
   }
 
   Widget _buildTodoItem(Task task, AppDatabase database) {
-    return Slidable(
-      actionPane: SlidableDrawerActionPane(),
-      secondaryActions: [
-        IconSlideAction(
-          icon: Icons.delete,
-          caption: "Delete",
-          color: Colors.red,
-          onTap: () {
-            database.deleteTask(task);
-          },
-        ),
-      ],
-      child: Card(
-        child: GestureDetector(
-          child: CheckboxListTile(
-            title: Text(task.name),
-            subtitle: Text(task.description),
-            value: task.isComplete,
-            onChanged: (bool value) {
-              database.updateTask(task.copyWith(isComplete: value));
-            },
-          ),
-          onTap: () {
-            Navigator.pushNamed(context, TaskDetailsScreen.routeName,
-                arguments: TaskDetailsArguments(task.id));
-          },
-        ),
-      ),
-    );
+    return TodoItem(task, database);
   }
 }
